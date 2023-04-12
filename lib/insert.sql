c2d938d0e6c829f1bbdd95449845c38d40b66190
Holden Gerner <hgerner19@gmail.com>
2023-04-12 11:37:51 -0600

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (1,'Mr. Chocolate',4,'M','brown','calm',0);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (2,'Rowdy',4,'F','brown','calm',1);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (3,'Tabitha',9,'F','brown','calm',0);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (4,'Sergeant Brown',10,'M','brown','calm',1);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (5,'Melissa',4,'M','black','aggressive',1);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (6,'Grinch',4,'M','brown','calm',1);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (7,'Wendy',10,'M','brown','calm',1);
+
+INSERT INTO bears (id,name,age,sex,color,temperament,alive)
+VALUES (8,NULL,1,'M','brown','calm',0);
+
+
